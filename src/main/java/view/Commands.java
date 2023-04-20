b8deeaf85b3bf77bc760bae1b856889776e5ae81
Nika-Ghaderi <nika_ghaderi@yahoo.com>
2023-04-21 00:34:55 +0430

    removing "forgot"

--- src/main/java/view/Commands.java
+++ src/main/java/view/Commands.java
@@ -7,6 +7,7 @@ public enum Commands {
     CREATE_USER("^\\s*user\\s+create\\s+(?<data>.+)$"),
     SECURITY_QUESTION_PICK("^question pick -q (?<number>\\d) -a (?<answer>.+) -c (?<answerConfirm>.+)$"),
     PASSWORD_FORGOT("^\\s*forgot\\s+my\\s+password\\s+-u\\s+(?<username>(\"[^\"]*\")|\\S*)$");
+
     public final String regex;
 
     Commands(String regex) {
