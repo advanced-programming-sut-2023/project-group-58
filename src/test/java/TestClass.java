9e945ff00fe67ce43db5942e996bc8da05cda327
Nika-Ghaderi <nika_ghaderi@yahoo.com>
2023-04-21 23:32:16 +0430

    some changes

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,16 +2,27 @@ import controller.RegisterMenuController;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.function.Executable;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class TestClass {
 
     RegisterMenuController registerMenuController = new RegisterMenuController();
     @Test
+    @DisplayName("Correcting the input Strings with extra double quotes.")
     public void correctingDoubleQuotes() {
         String testing = new String();
         testing = "\"jhkjhh\"";
+//        Assertions.assertThrows(NullPointerException.class, new Executable() {
+//            @Override
+//            public void execute() throws Throwable {
+//
+//            }
+//        })
+
         Assertions.assertEquals(registerMenuController.correctDoubleQuotation(testing),"\"jhkjhh\"");
         testing = "";
         Assertions.assertEquals(registerMenuController.correctDoubleQuotation(testing),"");
@@ -19,7 +30,10 @@ public class TestClass {
         Assertions.assertEquals(registerMenuController.correctDoubleQuotation(testing),"jhkj  hh");
         testing = "jhkj  hh";
         Assertions.assertEquals(registerMenuController.correctDoubleQuotation(testing),"jhkj  hh");
+
     }
+    //auto generate equals
+
 
     @BeforeEach
     public void settingUp() {
