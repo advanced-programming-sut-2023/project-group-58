a070ab3c866aa156af9eadff2f2bd49231bc2c0e
EPO004 <professorfb83@gmail.com>
2023-05-15 00:27:18 +0430

    main cleared

--- src/main/java/model/units/NotTroop.java
+++
@@ -1,9 +0,0 @@
-package model.units;
-
-import model.User;
-
-class NotTroop extends Unit{
-    public NotTroop(User master, UnitEnum type, int count, int primaryY, int primaryX) {
-        super(master,type,count,primaryY,primaryX);
-    }
-}
