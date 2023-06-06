52aaf64f7cc982378f6526842b6d181beae22433
Nika-Ghaderi <nika.ghadery@gmail.com>
2023-06-06 21:52:53 +0430

    working on register menu

--- src/main/java/view/LoginMenu.java
+++ src/main/java/view/LoginMenu.java
@@ -3,6 +3,7 @@ import controller.LoginMenuControl;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,23 +11,27 @@ import javafx.scene.control.*;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import view.controls.LoginRegisterMenuControl;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class LoginMenu extends Application {
     private static Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        LoginMenuControl loginMenuControl = new LoginMenuControl();
         stage = new Stage();
         URL url = LoginMenu.class.getResource("/FXML/firstMenu.fxml");
         BorderPane pane = FXMLLoader.load(url);
-
         Scene scene = new Scene(pane);
         stage.setScene(scene);
-        stage.setFullScreen(true);
+        //stage.setFullScreen(true);
         stage.show();
 
 
@@ -58,8 +63,8 @@ public class LoginMenu extends Application {
 //        stage.setFullScreen(true);
 //        stage.show();
     }
+
     private void addToVbox(VBox vBox){
-        LoginMenuControl loginMenuControl = new LoginMenuControl();
         Label label = GetStyle.label("Welcome to StrongHold!");
         TextField username = GetStyle.textField("username");
         username.focusedProperty().addListener(
--- src/main/java/view/controls/LoginRegisterMenuControl.java
+++ src/main/java/view/controls/LoginRegisterMenuControl.java
@@ -1,38 +1,51 @@
 package view.controls;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 import view.LoginMenu;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 
 public class LoginRegisterMenuControl {
 
     public TextField username;
     public PasswordField password;
+    public TextField nickname;
+    public TextField email;
 
-    public void login(MouseEvent mouseEvent) throws IOException {
+    public void login() throws IOException {
         URL url = LoginMenu.class.getResource("/FXML/loginMenu.fxml");
         BorderPane pane = FXMLLoader.load(url);
         Scene scene = new Scene(pane);
         LoginMenu.getStage().setScene(scene);
-        LoginMenu.getStage().setFullScreen(true);
+        //LoginMenu.getStage().setFullScreen(true);
         LoginMenu.getStage().show();
     }
 
-    public void register(MouseEvent mouseEvent) {
+    public void register() throws IOException {
+        URL url = LoginMenu.class.getResource("/FXML/registerMenu.fxml");
+        BorderPane pane = FXMLLoader.load(url);
+        Scene scene = new Scene(pane);
+        LoginMenu.getStage().setScene(scene);
+        //LoginMenu.getStage().setFullScreen(true);
+        LoginMenu.getStage().show();
     }
 
     public void exit(MouseEvent mouseEvent) {
-        LoginMenu.getStage().close();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Attention");
+        alert.setHeaderText("Exit");
+        alert.setContentText("Are you sure you want to leave?");
+
+        Optional<ButtonType> option = alert.showAndWait();
+        if (option.get() == ButtonType.OK) {
+            javafx.application.Platform.exit();
+        }
     }
 
     public void forgotpass(MouseEvent mouseEvent) {
@@ -40,5 +53,11 @@ public class LoginRegisterMenuControl {
     }
 
     public void back(MouseEvent mouseEvent) throws Exception {
+        URL url = LoginMenu.class.getResource("/FXML/firstMenu.fxml");
+        BorderPane pane = FXMLLoader.load(url);
+        Scene scene = new Scene(pane);
+        LoginMenu.getStage().setScene(scene);
+        LoginMenu.getStage().show();
     }
+
 }
